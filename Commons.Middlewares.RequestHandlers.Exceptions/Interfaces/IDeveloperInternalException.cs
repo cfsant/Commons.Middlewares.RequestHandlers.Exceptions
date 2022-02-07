962829532691293df1b04165f336ff231feebf58
CAIO FISCHER <caiofischer@infosiscs.com>
2022-02-07 16:20:22 -0300

    fix 'code smells'

--- Commons.Middlewares.RequestHandlers.Exceptions/Interfaces/IDeveloperInternalException.cs
+++ Commons.Middlewares.RequestHandlers.Exceptions/Interfaces/IDeveloperInternalException.cs
@@ -4,7 +4,9 @@ namespace Commons.Middlewares.RequestHandlers.Exceptions.Interfaces
 {
     public interface IDeveloperInternalException : IInternalException
     {
+#nullable enable
         Exception? InnerException { get; set; }
+#nullable disable
         string StackTrace { get; set; }
     }
 }
